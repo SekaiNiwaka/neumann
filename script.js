2ad6e35c62ccc44b06995f34a0e9944099eb4114
SekaiNiwaka <kantaro061026@outlook.jp>
2026-06-22 11:15:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -505,7 +505,7 @@ function setup() {
   wmap = [];
   sei = 0, mis = 0, hus = 0, kon = 0;
   tb = 0;
-  zik.innerText = '時間追加　：　0';
+  //zik.innerText = '時間追加　：　0';
   zik.style.color = 'gray';
   let p = document.getElementById('per');
   p.style.color = 'gray';
